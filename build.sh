d2ada6f8bdac52a22c4cd7b03e5834ed8e279db9
aoverb <250614271@qq.com>
2026-02-26 18:49:27 +0800

    完善用户态程序构建和打包逻辑

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@ make
 cd ..
 # 复制内核
 cp kernel/lolios.kernel isodir/boot/lolios.bin
+cp user/hello_world.bin isodir/boot/hello_world.bin
 
 # 创建 GRUB 配置文件
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "LoliOS" {
 	multiboot /boot/lolios.bin
+    module /boot/hello_world.bin
 }
 EOF
 
