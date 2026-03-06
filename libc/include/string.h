d43dfb5f46e4149ff8762ef1a5ae617b3b2059d8
aoverb <250614271@qq.com>
2026-03-06 23:03:18 +0800

    实现memcmp工具函数

--- libc/include/string.h
+++ libc/include/string.h
@@ -11,6 +11,7 @@ size_t strlen(const char*);
 int strcmp(const char*, const char*);
 int strncmp(const char* str1, const char* str2, size_t n);
 void* memcpy(void* __restrict, const void* __restrict, size_t);
+int memcmp(const void *s1, const void *s2, size_t n);
 void* memset(void*, int, size_t);
 char* strcpy(char* dest, const char* src);
 char* strncpy(char* dest, const char* src, size_t n);
