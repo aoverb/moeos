04aeb5050dcba46fd2ff71c86998424773561c5b
aoverb <250614271@qq.com>
2026-03-18 19:35:57 +0800

    键盘驱动添加特殊键识别

--- kernel/driver/keyboard.cpp
+++ kernel/driver/keyboard.cpp
@@ -18,11 +18,53 @@ const char scancode_to_ascii_table[128][2] = {
     /* 0x38 - 0x3F */ {0,0}, {' ',' '}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}
 };
 
-bool is_shift_pressed = false;
-bool is_ctrl_pressed = false;
+static bool is_shift_pressed = false;
+static bool is_ctrl_pressed = false;
+static bool e0_prefix = false;
+
+/* 发送 VT100 转义序列：\x1b [ <suffix> */
+static void send_escape_seq(const char* seq) {
+    while (*seq) {
+        terminal_input(*seq++);
+    }
+}
+
+/* 处理 0xE0 前缀后的第二个扫描码 */
+static void handle_e0_scancode(uint8_t scancode) {
+    if (scancode & KEY_RELEASED_MASK) {
+        return;
+    }
+
+    switch (scancode) {
+    case 0x48: send_escape_seq("\x1b[A");   break;  /* 上 */
+    case 0x50: send_escape_seq("\x1b[B");   break;  /* 下 */
+    case 0x4D: send_escape_seq("\x1b[C");   break;  /* 右 */
+    case 0x4B: send_escape_seq("\x1b[D");   break;  /* 左 */
+    case 0x47: send_escape_seq("\x1b[H");   break;  /* Home */
+    case 0x4F: send_escape_seq("\x1b[F");   break;  /* End */
+    case 0x53: send_escape_seq("\x1b[3~");  break;  /* Delete */
+    case 0x52: send_escape_seq("\x1b[2~");  break;  /* Insert */
+    case 0x49: send_escape_seq("\x1b[5~");  break;  /* Page Up */
+    case 0x51: send_escape_seq("\x1b[6~");  break;  /* Page Down */
+    default:
+        break;
+    }
+}
 
 void keyboard_interrupt_handler(registers* /* regs */) {
     uint8_t scancode = hal_inb(0x60);
+
+    if (scancode == 0xE0) {
+        e0_prefix = true;
+        return;
+    }
+
+    if (e0_prefix) {
+        e0_prefix = false;
+        handle_e0_scancode(scancode);
+        return;
+    }
+
     if (scancode & KEY_RELEASED_MASK) {
         uint8_t released = scancode ^ KEY_RELEASED_MASK;
         if (released == 0x2A || released == 0x36)
